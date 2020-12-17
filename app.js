c7adf5ac7239587cb74205b522ce5fe8907a351a
Nitinjha121 <nitinjha5121@gmail.com>
2020-12-17 17:44:58 +0530

    Color Change Of Circle

---
+++ app.js
@@ -0,0 +1,23 @@
+let blue=document.getElementById("blue");
+let red=document.getElementById("red");
+let green=document.getElementById("green");
+let aqua=document.getElementById("aqua");
+let coral=document.getElementById("coral");
+let circle=document.getElementById("circle");
+        
+        
+blue.onmouseover=function(){
+    circle.style.backgroundColor="blue";
+}  
+red.onmouseover=function(){
+    circle.style.backgroundColor="red";
+}
+green.onmouseover=function(){
+    circle.style.backgroundColor="green";
+}
+aqua.onmouseover=function(){
+    circle.style.backgroundColor="aqua";
+}
+coral.onmouseover=function(){
+    circle.style.backgroundColor="coral";
+}
